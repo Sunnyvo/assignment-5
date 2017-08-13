4b2cbe1484728d40f906f3e27df411cad51595f9
Sunny <votanphu.2101@gmail.com>
2017-08-13 20:29:19 +0700

    fix bug

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,11 +1,13 @@
 class FriendshipsController < ApplicationController
   def create
     current_user.friendships.create!(friendship_params)
+    redirect_back(fallback_location: root_path)
   end
 
   def destroy
     another_user = User.find params[:user_id]
     current_user.friends.delete(another_user)
+    redirect_back(fallback_location: root_path)
   end
 
   def friendship_params
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,8 +44,8 @@ class UsersController < ApplicationController
       @users= current_user.friends
     end
   end
+  
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password, :image_url)
   end
