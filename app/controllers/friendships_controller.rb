3704d5a0abc505fe1182c703b77b0988dcd8426f
Sunny <votanphu.2101@gmail.com>
2017-08-19 12:04:50 +0700

    ajax for addfriend and remove friend, add post

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,13 +1,21 @@
 class FriendshipsController < ApplicationController
   def create
-    current_user.friendships.create!(friendship_params)
-    redirect_back(fallback_location: root_path)
+    friendship = current_user.friendships.create!(friendship_params)
+    @item = friendship.friend
+    respond_to do |f|
+      f.html {redirect_back(fallback_location: root_path)}
+      f.js {render "add_friend"}
+    end
   end
 
   def destroy
     another_user = User.find params[:user_id]
     current_user.friends.delete(another_user)
-    redirect_back(fallback_location: root_path)
+    @item = another_user
+    respond_to do |f|
+      f.html {redirect_back(fallback_location: root_path)}
+      f.js {render "remove_friend"}
+    end
   end
 
   def friendship_params
